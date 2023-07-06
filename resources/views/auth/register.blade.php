9d333bee2897d22b3f7d2e19c530ef43dbf8960b
Adrian Alejandro Cruz Alanis <alejandrocruzalanis@gmail.com>
2023-07-05 21:27:52 -0600

    4 commit

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -66,6 +66,10 @@
                                 <button type="submit" class="btn btn-primary">
                                     {{ __('Register') }}
                                 </button>
+
+                                <a class="btn btn-link" href="{{ route('login') }}">
+                                    {{ __('Deseas iniciar sesion?') }}
+                                </a>
                             </div>
                         </div>
                     </form>
